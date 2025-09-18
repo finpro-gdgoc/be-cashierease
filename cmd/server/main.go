847442176813f6970607bc906d2876fbc7c39f4b
Depo S. H <141189770+dxdeposh@users.noreply.github.com>
2025-09-18 19:04:48 +0700

    refactor(server): integrate produk routes into main application

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"cashierease/config"
+	"cashierease/internal/routes"
 
 	"github.com/gin-gonic/gin"
 )
@@ -12,6 +13,10 @@ func init() {
 
 func main() {
 	router := gin.Default()
+
+	apiV1 := router.Group("/api/v1")
+
+	routes.SetupProdukRoutes(apiV1)
 
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
