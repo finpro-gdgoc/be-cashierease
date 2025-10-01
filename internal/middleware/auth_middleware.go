6797554d55cd66bc47daeff3765b87d2681a8e7b
Depo S. H <141189770+dxdeposh@users.noreply.github.com>
2025-10-01 13:59:56 +0700

    feat(security): implement JWT middleware to protect routes

---
+++ internal/middleware/auth_middleware.go
@@ -0,0 +1,34 @@
+package middleware
+
+import (
+	"cashierease/internal/utils"
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+		if authHeader == "" {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Authorization header required"})
+			return
+		}
+
+		// Format header: "Bearer <token>"
+		parts := strings.Split(authHeader, " ")
+		if len(parts) != 2 || parts[0] != "Bearer" {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Authorization header format must be Bearer {token}"})
+			return
+		}
+
+		token, err := utils.ValidateToken(parts[1])
+		if err != nil || !token.Valid {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Invalid or expired token"})
+			return
+		}
+
+		c.Next()
+	}
+}
--- internal/routes/produk_routes.go
+++ internal/routes/produk_routes.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"cashierease/internal/handlers"
+	"cashierease/internal/middleware"
 
 	"github.com/gin-gonic/gin"
 )
@@ -9,10 +10,11 @@ import (
 func SetupProdukRoutes(router *gin.RouterGroup) {
 	produkRoutes := router.Group("/produk")
 	{
-		produkRoutes.POST("/", handlers.CreateProduk)
 		produkRoutes.GET("/", handlers.GetAllProduk)
 		produkRoutes.GET("/:id", handlers.GetProdukById)
-		produkRoutes.PATCH("/:id", handlers.UpdateProduk)
-		produkRoutes.DELETE("/:id", handlers.DeleteProduk)
+
+		produkRoutes.POST("/", middleware.AuthMiddleware(), handlers.CreateProduk)
+		produkRoutes.PATCH("/:id", middleware.AuthMiddleware(), handlers.UpdateProduk)
+		produkRoutes.DELETE("/:id", middleware.AuthMiddleware(), handlers.DeleteProduk)
 	}
 }
