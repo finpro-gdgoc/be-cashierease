bb2a399395332febdd3e759197b5dec21a3e0616
Depo S. H <141189770+dxdeposh@users.noreply.github.com>
2025-10-30 08:43:57 +0700

    feat(user): add user repository

--- internal/repositories/user_repository.go
+++ internal/repositories/user_repository.go
@@ -14,3 +14,23 @@ func FindUserByNomorPegawai(nomorPegawai string) (models.User, error) {
 	err := config.DB.Where("nomor_pegawai = ?", nomorPegawai).First(&user).Error
 	return user, err
 }
+
+func GetAllUsers() ([]models.User, error) {
+	var users []models.User
+	err := config.DB.Find(&users).Error
+	return users, err
+}
+
+func GetUserByID(id uint) (models.User, error) {
+	var user models.User
+	err := config.DB.First(&user, id).Error
+	return user, err
+}
+
+func UpdateUser(user *models.User) error {
+	return config.DB.Save(user).Error
+}
+
+func DeleteUser(id uint) error {
+	return config.DB.Delete(&models.User{}, id).Error
+}
