5a3848d33081993440007afa7715b44b2d0e9a7e
Depo S. H <141189770+dxdeposh@users.noreply.github.com>
2025-10-30 08:58:37 +0700

    feat(produk): add produk handlers for CRUD and image upload

--- internal/handlers/produk_handler.go
+++ internal/handlers/produk_handler.go
@@ -3,8 +3,12 @@ package handlers
 import (
 	"cashierease/internal/models"
 	"cashierease/internal/repositories"
+	"fmt"
 	"net/http"
+	"os"
+	"path/filepath"
 	"strconv"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gosimple/slug"
@@ -91,4 +95,49 @@ func DeleteProduk(c *gin.Context) {
 		return
 	}
 	c.JSON(http.StatusOK, gin.H{"message": "Produk deleted successfully"})
+}
+
+func UploadGambarProduk(c *gin.Context) {
+	id, err := strconv.ParseUint(c.Param("id"), 10, 32)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid ID"})
+		return
+	}
+
+	produk, err := repositories.GetProdukById(uint(id))
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Produk not found"})
+		return
+	}
+
+	file, err := c.FormFile("gambar_produk")
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "File 'gambar_produk' not found in request"})
+		return
+	}
+
+	uploadDir := "./public/uploads"
+	if err := os.MkdirAll(uploadDir, os.ModePerm); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create upload directory"})
+		return
+	}
+
+	extension := filepath.Ext(file.Filename)
+	filename := fmt.Sprintf("produk-%d-%d%s", id, time.Now().Unix(), extension)
+	savePath := filepath.Join(uploadDir, filename)
+
+	if err := c.SaveUploadedFile(file, savePath); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to save file"})
+		return
+	}
+
+	urlPath := "/public/uploads/" + filename
+
+	produk.GambarProduk = urlPath
+	if err := repositories.UpdateProduk(&produk); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to update product path in database"})
+		return
+	}
+
+	c.JSON(http.StatusOK, produk)
 }
