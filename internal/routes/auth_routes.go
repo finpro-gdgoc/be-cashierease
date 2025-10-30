b888ede62b432b70e97d9269321ab2f1d314c7bd
Depo S. H <141189770+dxdeposh@users.noreply.github.com>
2025-10-30 08:44:21 +0700

    feat(routes): add auth routes for login, register, and user management

--- internal/routes/auth_routes.go
+++ internal/routes/auth_routes.go
@@ -2,6 +2,8 @@ package routes
 
 import (
 	"cashierease/internal/handlers"
+	"cashierease/internal/middleware"
+	"cashierease/internal/models"
 
 	"github.com/gin-gonic/gin"
 )
@@ -11,5 +13,15 @@ func SetupAuthRoutes(router *gin.RouterGroup) {
 	{
 		authRoutes.POST("/register", handlers.Register)
 		authRoutes.POST("/login", handlers.Login)
+
+		adminOnly := authRoutes.Group("/")
+		adminOnly.Use(middleware.AuthMiddleware())
+		adminOnly.Use(middleware.RoleMiddleware(models.AdminRole))
+		{
+			adminOnly.GET("/", handlers.GetAllUsers)
+			adminOnly.GET("/:id", handlers.GetUserByID)
+			adminOnly.PATCH("/:id", handlers.UpdateUser)
+			adminOnly.DELETE("/:id", handlers.DeleteUser)
+		}
 	}
 }
